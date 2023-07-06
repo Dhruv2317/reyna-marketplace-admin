ca1f5cef56bc063788df846acddefd3dcec1e70a
Girish Suthar <girish.suthar@reynasolutions.com>
2023-07-06 14:10:29 +0530

    interceptor implement, login changes

--- src/app/constants/MarketplaceApiEndpoints.ts
+++ src/app/constants/MarketplaceApiEndpoints.ts
@@ -3,7 +3,7 @@ export class MarketplaceApiEndpoints {
 
 
   // auth api endpoints
-  public static LOGIN='api/Auth/LoginAsync';
+  public static LOGIN='api/Auth/UserLogin';
   public static LOGOUT='api/auth/logout';
   public static ME='api/auth/me';
   public static FORGOT_PASSWORD='api/auth/forgotpassword';
